10c820456bc0ab84fa5e9c35ddbd4e407808c489
Luiz <argentinaluiz@gmail.com>
2018-09-20 15:51:37 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    // nameControl = new FormControl('');
+    // emailControl = new FormControl('');
+    //
+    // showError(control) {
+    //     return control.invalid && (control.touched || control.dirty);
+    // }
+    //
+    // hasError(control, error) {
+    //     return control.errors && control.errors.hasOwnProperty(error);
+    // }
+    //FormGroup é um agregado de FormControls
+
+    formGroup = new FormGroup({
+        name: new FormControl(''),
+        email: new FormControl(''),
+        body: new FormControl('')
+    });
+
+    showError(field) {
+        const control = this.formGroup.get(field);
+        return control.invalid && (control.touched || control.dirty);
+    }
+
+    hasError(field, error) {
+        const control = this.formGroup.get(field);
+        return control.errors && control.errors.hasOwnProperty(error);
+    }
 }
+
